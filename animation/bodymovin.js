d80269b18b023fef256800c10bcc6a7308155a98
ab <alberto.buzio@gmail.com>
2019-10-11 18:41:46 +0200

    Update bodymovin.js

--- animation/bodymovin.js
+++ animation/bodymovin.js
@@ -1,14 +1,7 @@
-var container = document.getElementById('anim_research');
-
-console.log(container);
-      // Set up our animation 
-
-      var animData = {
-          container: container,
-          renderer: 'svg',
-          autoplay: true,
-          loop: false,
-          path : 'data.json'
-      };
-
-var anim = bodymovin.loadAnimation(animData);
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('anim_research'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'animation/data.json'
+})
